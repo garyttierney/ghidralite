59410c7dbfc6ad84ee4149725c87988606a8845a
Gary Tierney <gary.tierney@fastmail.com>
2024-04-13 03:14:26 +0100

    Pass keyboard events to list from QuickSearch

--- src/main/kotlin/ui/search/QuickSearchResultsView.kt
+++ src/main/kotlin/ui/search/QuickSearchResultsView.kt
@@ -1,22 +1,19 @@
 package io.github.garyttierney.ghidralite.ui.search
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.*
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.background
+import androidx.compose.foundation.focusGroup
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -25,116 +22,89 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import io.github.garyttierney.ghidralite.framework.search.SearchResult
 import io.github.garyttierney.ghidralite.ui.internal.PreviewComponent
-import kotlinx.coroutines.launch
-import org.jetbrains.jewel.foundation.lazy.SelectableLazyColumn
-import org.jetbrains.jewel.foundation.lazy.SelectableLazyListState
-import org.jetbrains.jewel.foundation.lazy.SelectionMode
-import org.jetbrains.jewel.foundation.lazy.rememberSelectableLazyListState
+import org.jetbrains.jewel.foundation.lazy.*
+import org.jetbrains.jewel.foundation.lazy.tree.DefaultSelectableLazyColumnKeyActions
+import org.jetbrains.jewel.foundation.lazy.tree.KeyActions
 import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.ui.Outline
 import org.jetbrains.jewel.ui.component.*
-import org.jetbrains.jewel.ui.component.VerticalScrollbar
 import org.jetbrains.jewel.ui.theme.colorPalette
 import org.jetbrains.jewel.ui.theme.menuStyle
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun QuickSearch(
     items: List<SearchResult>,
     query: String,
     onQueryChanged: (String) -> Unit,
     onResultSelected: (SearchResult) -> Unit,
-    focusRequester: FocusRequester = FocusRequester()
+    focusRequester: FocusRequester = FocusRequester(),
+    listActions: KeyActions = remember { DefaultSelectableLazyColumnKeyActions(DefaultSelectableColumnKeybindings) }
 ) {
-    val borderColor = JewelTheme.globalColors.borders.normal
-    val bgColor = JewelTheme.menuStyle.colors.background
+    val itemListState = rememberSelectableLazyListState()
+    val itemKeys by derivedStateOf { items.map { SelectableLazyListKey.Selectable(it.uniqueKey) }.toList() }
 
-    val interactionSource = remember { MutableInteractionSource() }
-    val state = rememberSelectableLazyListState()
-    val scope = rememberCoroutineScope()
+    val handleListAction = { event: KeyEvent ->
+        val itemListHandler = listActions.handleOnKeyEvent(event, itemKeys, itemListState, SelectionMode.Single)
 
-    Column(modifier = Modifier.background(bgColor).focusGroup().border(1.dp, borderColor)) {
-        TextField(
-            query,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-            undecorated = true,
-            outline = Outline.None,
-            onValueChange = onQueryChanged,
-            modifier = Modifier.fillMaxWidth()
-                .focusRequester(focusRequester)
-                .focusable(true)
-                ,
-            placeholder = { Text("Type a query to begin searching") },
-            interactionSource = interactionSource,
-        )
-
-        QuickSearchResultList(items = items, onItemSelected = onResultSelected, interactionSource = interactionSource)
+        if (event.itemListHandler()) {
+            true
+        } else {
+            false
+        }
     }
+
+    Column(modifier = Modifier.onPreviewKeyEvent(handleListAction).focusRequester(focusRequester).focusGroup()) {
+        QuickSearchInput(query = query, onQueryChanged = onQueryChanged)
+
+        QuickSearchResultList(
+            items = items,
+            state = itemListState,
+            onItemSelected = onResultSelected,
+        )
+    }
+}
+
+@Composable
+fun QuickSearchInput(
+    query: String, onQueryChanged: (String) -> Unit
+) {
+    TextField(
+        query,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        undecorated = true,
+        outline = Outline.None,
+        onValueChange = { onQueryChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text("Type a query to begin searching") },
+    )
 }
 
 @Composable
 fun QuickSearchResultList(
     items: List<SearchResult>,
     onItemSelected: (SearchResult) -> Unit,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    state: SelectableLazyListState = rememberSelectableLazyListState()
+    state: SelectableLazyListState = rememberSelectableLazyListState(),
 ) {
+    val listTheme = Modifier.fillMaxSize().background(JewelTheme.menuStyle.colors.background)
+
     Box {
         SelectableLazyColumn(
             contentPadding = PaddingValues(2.dp),
             selectionMode = SelectionMode.Single,
-            interactionSource = interactionSource,
             state = state,
-            modifier = Modifier.fillMaxSize()
-                .background(JewelTheme.menuStyle.colors.background)
-                .focusable(interactionSource = interactionSource)
-                .indication(interactionSource = interactionSource, LocalIndication.current),
+            modifier = listTheme.focusable(),
         ) {
-
-
             items.forEach {
                 item(key = it.uniqueKey) {
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(4.dp),
-                        verticalAlignment = Alignment.Bottom,
-                        modifier = Modifier.fillMaxWidth().padding(4.dp)
-                            .then(
-                                when {
-                                    isSelected -> Modifier.background(JewelTheme.menuStyle.colors.itemColors.backgroundFocused)
-                                    !isSelected -> Modifier.background(JewelTheme.menuStyle.colors.itemColors.background)
-                                    else -> Modifier
-                                },
-                            )
-                    ) {
-                        Icon(
-                            "/nodes/method.svg",
-                            contentDescription = "Test",
-                            iconClass = SearchResult::class.java,
-                            modifier = Modifier.size(16.dp)
-                        )
-
-                        Text(
-                            AnnotatedString(
-                                text = it.name,
-                                spanStyles = it.fragments.map { range ->
-                                    AnnotatedString.Range(
-                                        SpanStyle(background = JewelTheme.colorPalette.yellow(3)),
-                                        range.startOffset,
-                                        range.startOffset + range.length
-                                    )
-                                }
-                            ),
-                            maxLines = 1,
-                            modifier = Modifier.weight(1f, false),
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    }
+                    QuickSearchResult(item = it)
                 }
             }
-
         }
 
         LaunchedEffect(items) {
-            state.lastActiveItemIndex = 0
+            state.scrollToItem(0)
+            state.lastActiveItemIndex = -1
         }
 
         VerticalScrollbar(
@@ -145,6 +115,45 @@ fun QuickSearchResultList(
 }
 
 
+@Composable
+fun SelectableLazyItemScope.QuickSearchResult(item: SearchResult) {
+    val rowTheme = Modifier.fillMaxWidth().padding(4.dp).then(
+        when {
+            isSelected -> Modifier.background(JewelTheme.menuStyle.colors.itemColors.backgroundFocused)
+            !isSelected -> Modifier.background(JewelTheme.menuStyle.colors.itemColors.background)
+            else -> Modifier
+        },
+    )
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.Bottom,
+        modifier = rowTheme
+    ) {
+        Icon(
+            "/nodes/method.svg",
+            contentDescription = "Test",
+            iconClass = SearchResult::class.java,
+            modifier = Modifier.size(16.dp)
+        )
+
+        val matchingRangeAnnotations = item.fragments.map { range ->
+            AnnotatedString.Range(
+                SpanStyle(background = JewelTheme.colorPalette.yellow(3)),
+                range.startOffset,
+                range.startOffset + range.length
+            )
+        }
+
+        Text(
+            AnnotatedString(text = item.name, spanStyles = matchingRangeAnnotations),
+            maxLines = 1,
+            modifier = Modifier.weight(1f, false),
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
 @Preview
 @Composable
 internal fun QuickSearchPreview() = PreviewComponent {
@@ -154,9 +163,5 @@ internal fun QuickSearchPreview() = PreviewComponent {
         )
     }
 
-    QuickSearch(
-        items = items.toList(),
-        query = "Search query",
-        onQueryChanged = {},
-        onResultSelected = {})
+    QuickSearch(items = items.toList(), query = "Search query", onQueryChanged = {}, onResultSelected = {})
 }
