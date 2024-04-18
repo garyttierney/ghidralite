f67af3da603525de7cc1f0d1ca096fdfb90103d9
Gary Tierney <gary.tierney@fastmail.com>
2024-04-18 21:15:28 +0100

    Remove old standalone code

--- ghidralite-core-ksp/build.gradle.kts
+++ ghidralite-core-ksp/build.gradle.kts
@@ -3,26 +3,8 @@ plugins {
     `ghidralite-kotlin-conventions`
 }
 
-group = "io.github.garyttierney"
-version = "unspecified"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     testImplementation(kotlin("test"))
     implementation(libs.ksp)
     implementation(kotlin("stdlib"))
-}
-
-sourceSets.main {
-    java.srcDirs("src/main/kotlin")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
 }
